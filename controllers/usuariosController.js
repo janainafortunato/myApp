e1f331e84df4938fe95d53ef5e7379c1ce104ae3
janainafortunato <janaina.projetos@live.com>
2021-04-05 15:17:15 -0300

    estudos de node.js

---
+++ controllers/usuariosController.js
@@ -0,0 +1,8 @@
+const usuarios = require('../models/usuarios');
+const usuariosController = {
+    index: (req, res) => {
+       return  res.send(usuarios.listarUsuarios());
+    }
+}
+
+module.exports = usuariosController;
---
+++ models/usuarios.js
@@ -0,0 +1,9 @@
+//const usuariosController = require("../controllers/usuariosController")
+
+const usuarios = {
+    listarUsuarios: () => {
+        return ['ana', 'geisiane', 'luiz', 'Mateus'];
+    }
+}
+
+module.exports = usuarios;
